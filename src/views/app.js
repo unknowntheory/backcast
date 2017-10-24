54773a01a6c9797b5cbda460aa80ac7236da3476
Daniel London <theryy408@gmail.com>
2017-10-24 10:31:47 -0700

    work in progress

--- src/views/app.js
+++ src/views/app.js
@@ -3,12 +3,20 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.videos = new Videos(window.exampleVideoData);
+    this.render();
+
   },
 
 
   render: function() {
+    console.log(this.videos);
+    console.log(this.videos.models);
     this.$el.html(this.template());
+    new VideoListView({ collection: this.videos, el: this.$('.list')
+
+    }).render();
+
     return this;
   },
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,10 +1,17 @@
 var VideoListView = Backbone.View.extend({
 
-
+  //el: '.list',
+  initialize: function() {
+    this.render();
+  },
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
+
+    // var greed = this.co  llection.map((video)=>{ return video.collection.models; });
+    
     return this;
+
   },
 
   template: templateURL('src/templates/videoList.html')
